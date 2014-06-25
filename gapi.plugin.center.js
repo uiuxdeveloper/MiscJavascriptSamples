d696d9c351662bd88a637faf586679642594c4a0
Curtis Janczak <cjanczak@nexon.net>
2014-06-25 10:11:47 -0700

    Commit initial assets

---
+++ gapi.plugin.center.js
@@ -0,0 +1,23 @@
+jQuery.fn.center = function() {
+    return this.each(function(){
+        var el = $(this);
+        var h = el.height();
+        var w = el.width();
+        var w_box = $(window).width();
+        var h_box = $(window).height(); 
+        var w_total = (w_box - w)/2;        //400
+
+        if( typeof el.attr('data-cntr-top') !== 'undefined' )
+            var h_total = el.attr('data-cntr-top');
+        else
+            var h_total = (h_box - h)/2;
+
+        var css = {"position": 'absolute', "left": w_total+"px", "top": h_total+"px"};
+        el.css(css)
+    });
+};
+
+$(document).ready(function() {
+    // center absolute positioned elements
+    $( '.cntr' ).center();
+});
